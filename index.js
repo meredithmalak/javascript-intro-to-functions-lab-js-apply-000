f76ed25461d65fb6694fa10b21dac1cd07a2169b
Meredith Malak <mer87099@gmail.com>
2020-04-28 05:32:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function logShout(string) {console.log ('HELLO')}
 function logWhisper(string) {console.log ('hello')}
 function sayHiToGrandma(string) {if (string =='hello') return "I can\'t hear you!";
 if (string =='HELLO') return "YES INDEED!"}
-if (string == "I love you, Grandma") return "I love you, too.";
+if (string == "I love you, too") return "I love you, too.";
