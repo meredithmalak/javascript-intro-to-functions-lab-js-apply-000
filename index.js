2d9576b409fc4140c71f9bd0b126533da01ef141
Meredith Malak <mer87099@gmail.com>
2020-04-28 05:03:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ function shout(string) { return string.toUpperCase() }
 function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log ('HELLO')}
 function logWhisper(string) {console.log ('I can\'t hear you')}
-var lowercase = "I can't hear you!";
 function sayHiToGrandma(string) {return string.toLowerCase()}
